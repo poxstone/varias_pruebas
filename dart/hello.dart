2cbc432b18ae3f95c2a2b3faa9d7d7ba8a18e8cd
developer <developer@instance.vnc>
2019-01-05 14:44:07 +0000

    update dart

---
+++ dart/hello.dart
@@ -0,0 +1,17 @@
+voiod main() {
+
+  var example5method = "Example5 sayIt";
+  class Example5Class {
+      sayIt() {
+            print(example5method);
+              }
+  }
+  example5() {
+      // Create an anonymous instance of the Example5Class and call the sayIt
+      // method on it.
+      new Example5Class().sayIt();
+  }
+
+
+
+}
